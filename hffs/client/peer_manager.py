97a0509b7b2bcc8aabf8c0d861790a6e42c7e10e
liuzhengrong <953175531@qq.com>
2024-06-20 15:58:23 +0800

    add: prompt when rm file use absolutely path add: validate ip and port when add/rm peer add: prompt when add/rm peer success

--- hffs/client/peer_manager.py
+++ hffs/client/peer_manager.py
@@ -1,6 +1,21 @@
+import logging
+import urllib3
+
 from typing import List
 from ..common.peer import Peer
 from .http_client import notify_peer_change, alive_peers
+
+
+def check_valid_ip_port(ip, port):
+    converted_url = "{}:{}".format(ip, port)
+
+    try:
+        parsed_url = urllib3.util.parse_url(converted_url)
+
+        if not parsed_url.host or not parsed_url.port:
+            raise ValueError("Should be not None!")
+    except Exception:
+        raise ValueError("Invalid IP or port format! IP: {}, port:{}".format(ip, port))
 
 
 class PeerManager:
@@ -10,12 +25,18 @@ class PeerManager:
         self._peer_store = peer_store
 
     def add_peer(self, ip, port=None):
-        peer = Peer(ip, port if port else self.DEFAULT_PORT)
+        peer_port = port if port else self.DEFAULT_PORT
+        check_valid_ip_port(ip, port)
+        peer = Peer(ip, peer_port)
         self._peer_store.add_peer(peer)
+        logging.info("Add success!")
 
     def remove_peer(self, ip, port=None):
-        peer = Peer(ip, port if port else self.DEFAULT_PORT)
+        peer_port = port if port else self.DEFAULT_PORT
+        check_valid_ip_port(ip, port)
+        peer = Peer(ip, peer_port)
         self._peer_store.remove_peer(peer)
+        logging.info("Remove success!")
 
     def get_peers(self) -> List[Peer]:
         return self._peer_store.get_peers()
